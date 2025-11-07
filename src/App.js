e9624b7e1d4df39aff7db98f3eb39c121cb581ce
Sander Verstraeten <r1051226@ucll.be>
2025-11-07 13:36:54 +0100

    Added style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import EmployeeList from "./EmployeeList";
+import "./theme.css";
 
 function App() {
   return (
-    <div>
-      <h1>Employee Info Display Tool</h1>
-      <EmployeeList />
+    <div className="app-shell">
+      <header className="app-header">
+        <div className="header-text">
+          <h1>Employee Info Display Tool</h1>
+        </div>
+      </header>
+
+      <main className="app-content">
+        <EmployeeList />
+      </main>
     </div>
   );
 }
